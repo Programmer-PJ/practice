26ae0a380d40d02a9ece72a6c6b6aa9ac9e8c705
Programmer-PJ <playerpj@foxmail.com>
2018-05-05 16:51:16 +0800

    004_complete

---
+++ 004/count_word.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+
+
+import re
+import pandas
+
+
+def catch(file):
+	with open(file, 'r') as f:
+		s = f.read()
+		words = re.findall(r'[a-zA-Z0-9]+', s)
+		return ''.join(words)
+
+def count(words):
+	c = {}
+	for i in words:
+		i = i.lower()
+		if i in c:
+			c[i] += 1
+		else:
+			c[i] = 1
+	return c
+
+
+if __name__ == '__main__':
+	w = catch('test.txt')
+	c = count(w)
+	s = pandas.Series(c)
+	print(s)
